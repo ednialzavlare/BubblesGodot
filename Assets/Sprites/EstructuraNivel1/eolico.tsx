24458f89b342b5bc7c81d792f61a8d37d6e85002
Daniel Alvarez <125082787+ednialzavlare@users.noreply.github.com>
2025-02-07 00:30:38 -0600

    Assets

---
+++ Assets/Sprites/EstructuraNivel1/eolico.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="eolico" tilewidth="1920" tileheight="1920" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="altar.png" width="1920" height="1080"/>
+ </tile>
+ <tile id="1">
+  <image source="altarroto.png" width="1920" height="1080"/>
+ </tile>
+ <tile id="2">
+  <image source="columna.png" width="1080" height="1920"/>
+ </tile>
+ <tile id="3">
+  <image source="columnatiradarota.png" width="1920" height="1080"/>
+ </tile>
+ <tile id="4">
+  <image source="objeto.png" width="1920" height="1080"/>
+ </tile>
+ <tile id="5">
+  <image source="pisocompleto.png" width="1920" height="1080"/>
+ </tile>
+ <tile id="6">
+  <image source="pisoroto.png" width="1920" height="1080"/>
+ </tile>
+ <tile id="7">
+  <image source="plataforma.png" width="1920" height="1080"/>
+ </tile>
+ <tile id="8">
+  <image source="plataformarota.png" width="1920" height="1080"/>
+ </tile>
+ <tile id="9">
+  <image source="plataformarota1.png" width="1920" height="1080"/>
+ </tile>
+ <tile id="10">
+  <image source="plataformarota2.png" width="1920" height="1080"/>
+ </tile>
+ <tile id="11">
+  <image source="plataformarota3.png" width="1920" height="1080"/>
+ </tile>
+</tileset>
---
+++ Scripts/menu_hub_world.gd
@@ -0,0 +1,34 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+
+
+func _on_opciones_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menus/menu_opciones.tscn")
+	# pass # Replace with function body.
+
+
+func _on_regresar_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menus/menu_inicio.tscn")
+	# pass # Replace with function body.
+
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()
+	# pass # Replace with function body.
+
+
+func _on_nivel_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Niveles/nivel_1.tscn")
+	
+	# pass # Replace with function body.
