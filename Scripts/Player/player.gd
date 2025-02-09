50f56ff3c60cee6171df1f24aeb49c92b739dc83
Daniel Alvarez <125082787+ednialzavlare@users.noreply.github.com>
2025-02-09 07:01:27 -0600

    Pausa y audio

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D  # Adjust path if needed
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -14,11 +14,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		sprite.flip_h = direction < 0  # Flip sprite when moving left
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
